da428855d098c858cf4c752e1cb0954d22d2adde
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-23 18:07:39 +1000

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Jesse Russell on 23/6/20.
+//  Copyright © 2020 Jesse Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
